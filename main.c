6206c7c12f8848305f10b3fac75bd6115d98313f
Luca <luca.niermont@gmail.com>
2024-08-25 13:00:12 +0200

    Initial Structure

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 // Piece Table Implementation in C.
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct{
+  unsigned int start;
+  size_t length;
+  bool isAdd;
+} Piece;
+
+typedef struct{
+  char *add;
+  const char original[];
+  
+} PieceTable;
